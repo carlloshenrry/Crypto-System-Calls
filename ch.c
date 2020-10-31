239c5a8f32f293fb59a8c6619bd117b1fff90da8
Carlos Henrique <38138832+carlloshenrry@users.noreply.github.com>
2020-10-31 00:07:37 -0300

    Add files via upload
    Project with the functions of reading and writing files + crypt working. With the participation of Hiagomoa

---
+++ ch.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define PATH "./arquivo.txt"
+
+int main()
+{  
+char vet[32];
+char msg[16];
+int i;
+    int fd = open(PATH, O_CREAT | O_WRONLY | O_TRUNC, 0666);
+    printf("Invoking 'listProcessInfo' system call\n");
+
+   printf("Digite a sua msg.\n");
+   scanf("%[^\n]s",msg);
+	
+	for(i=strlen(msg); i<16;i++){
+	msg[i]='0';	
+	}
+	msg[16]='\0';
+         
+    ssize_t ret_status = syscall(333, fd, msg , 32); 
+
+    fd = open(PATH, O_RDONLY| O_CREAT, 0666);
+
+         ret_status = syscall(334, fd, vet, 32);
+
+	printf("Foiii: %s\n", vet);
+    if(ret_status == 0) 
+         printf("System call 'listProcessInfo' executed correctly. Use dmesg to check processInfo\n");
+    
+    else 
+         printf("System call 'listProcessInfo' did not execute as expected\n");
+         
+     return 0;
+}
